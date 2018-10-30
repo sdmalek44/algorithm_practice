94acd7db23cf03a6ef67a67aed268fde37c1fe4f
Stephen Malek <malek.stephen44@gmail.com>
2018-10-29 20:37:04 -0600

    initialized git repo

---
+++ test/algorithem_practice_test.rb
@@ -0,0 +1,15 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/algorithm-practice.rb'
+require 'simplecov'
+# assert_equal expected, actual
+# assert boolean
+# assert_instance_of expected, actual
+
+SimpleCov.start
+
+class AlgorithemTest < Minitest::Test
+  def test_it_does_shit
+
+  end
+end
