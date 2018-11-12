fa7b4ce78d11c4ddd168bc82c072cc115a208180
Stephen Malek <malek.stephen44@gmail.com>
2018-11-12 13:52:36 -0700

    Add test for highest product of three integers

---
+++ test/highest_product_test.rb
@@ -0,0 +1,15 @@
+require './test/test_helper.rb'
+require './lib/highest_product_of_integers'
+
+class HighestProductTest < Minitest::Test
+  def test_it_gets_highest_product
+    expected = 8 * 7 * 9
+    input = [8, 4, 7, 3, 9]
+    assert_equal expected, array_of_ints(input)
+  end
+  def test_can_take_negatives_product
+    expected = 4 * -3 * -7
+    input = [-8, 4, -7, -3, -9]
+    assert_equal expected, array_of_ints(input)
+  end
+end
