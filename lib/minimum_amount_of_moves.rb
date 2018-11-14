2e3a6e99982a01b466eeec404d2dc7ea4ce2570a
Stephen Malek <malek.stephen44@gmail.com>
2018-11-14 15:30:46 -0700

    Add comments for directions

--- lib/minimum_amount_of_moves.rb
+++ lib/minimum_amount_of_moves.rb
@@ -1,3 +1,11 @@
+# Function Description
+#
+# Complete the function minimumBribes in the editor below. It must print an integer representing the minimum number of bribes necessary, or Too chaotic if the line configuration is not possible.
+#
+# minimumBribes has the following parameter(s):
+#
+# q: an array of integers
+
 def minimumBribes(q)
     moves = 0
     q.each_with_index do |num, index|
