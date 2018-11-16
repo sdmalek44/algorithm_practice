3651a08c556ca2e6eeb1d1f564b8803d61fe5429
Stephen Malek <malek.stephen44@gmail.com>
2018-11-16 14:28:51 -0700

    Test minimum swaps method

--- lib/minimum_swaps.rb
+++ lib/minimum_swaps.rb
@@ -3,10 +3,7 @@ def minimumSwaps(arr)
   swaps = 0
   arr.each_with_index do |num, i|
     if num != i + 1
-      t = i
-      while arr[t] != i + 1
-        t += 1
-      end
+      t = arr.index(i + 1)
       temp = arr[t]
       arr[t] = arr[i]
       arr[i] = temp
