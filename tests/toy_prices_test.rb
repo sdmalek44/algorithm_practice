54057beaa883f93d3e585675dafa441ad223d56f
stephen <malek.stephen44@gmail.com>
2018-11-26 15:42:15 -0700

    Add another test case for toy prices

--- tests/toy_prices_test.rb
+++ tests/toy_prices_test.rb
@@ -7,5 +7,9 @@ class ToyPricesTest < Minitest::Test
     prices = [1, 12, 5, 111, 200, 1000, 10]
     k = 50
     assert_equal 4, maximum_toys(prices, k)
+
+    prices = [3, 7, 2, 9, 4]
+    k = 15
+    assert_equal 3, maximum_toys(prices, k)
   end
 end
