01ba66b37e81fdc28dd680dd40807f78982e6e24
stephen <malek.stephen44@gmail.com>
2018-11-29 20:11:21 -0700

    Update algorithm to work for larger sample sets

--- lib/array_manipulation.rb
+++ lib/array_manipulation.rb
@@ -1,15 +1,18 @@
 def arrayManipulation(n, queries)
-    zeros = []
-    query_len = queries.length
-    highest = 0
-    for i in 1..n
-        num = 0
-        for j in 0...query_len
-            if (queries[j][0]..queries[j][1]).include? i
-                num += queries[j][2]
-            end
-        end
-        highest = num if num > highest
+    arr = []
+    max = 0
+    n.times { arr.push(0) }
+    for i in 0...queries.length
+      arr[queries[i][0] - 1] += queries[i][2]
+      if queries[i][1] < arr.length
+        arr[queries[i][1]] -= queries[i][2]
+      end
     end
-    highest
+    for j in 1...n
+      arr[j] += arr[j - 1]
+    end
+    for k in 0...arr.length
+      max = max > arr[k] ? max : arr[k]
+    end
+    max
 end
